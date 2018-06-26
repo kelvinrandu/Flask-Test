fc3a30b71244c896bf20ed44793d79b9525b0ccf
kelvinrandu <randukelvin@gmail.com>
2018-06-26 20:21:02 +0300

    routes and templates

---
+++ data.py
@@ -0,0 +1,26 @@
+def Articles():
+	articles = [
+		{
+		 	'id': 1,
+			'title':' Article one',
+			'body':'This represents the body of the first article',
+			'author':'Kelvin Gates',
+			'create_date':'04-25-2018'
+		},
+		{
+			'id': 2,
+			'title':' Article two',
+			'body':'This represents the body of the second article',
+			'author':'Tom Brady',
+			'create_date':'04-25-2018'
+		},
+		{
+		 	'id': 3,
+			'title':' Article three',
+			'body':'This represents the body of the third article',
+			'author':'Mary Jane ',
+			'create_date':'04-25-2018'
+		}
+
+	]
+	return articles
